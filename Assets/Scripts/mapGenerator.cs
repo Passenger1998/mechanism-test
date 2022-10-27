da121075bf3283993b2d2154550e5a72bd2c2cf5
Passenger1998 <98896021+Passenger1998@users.noreply.github.com>
2022-10-27 17:58:46 +0100

    alex_mechanism_workingbackup

--- Assets/Scripts/mapGenerator.cs
+++ Assets/Scripts/mapGenerator.cs
@@ -4,34 +4,22 @@ using UnityEngine;
 
 public class mapGenerator : MonoBehaviour
 {
-    //this is where prefab maps is added
-    public GameObject[] prefabMaps;
-    public GameObject player, trigger_back, trigger_front;
+    public GameObject backTrigger, frontTrigger;
 
-    bool whetherOnMap;
-
-    BoxCollider onMap;
-
-    void onTriggerEnter(Collider onMap)
-    {
-        whetherOnMap = true;
-    }
-
-   
-    void activateMapGenerator()
-    {
-        
-    }
+    GameObject backMap, frontMap;
 
     // Start is called before the first frame update
+
+    
+
     void Start()
     {
-        onMap = GetComponent<BoxCollider>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }

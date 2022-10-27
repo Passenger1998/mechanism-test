d9e58d15bb9345c9d19b20d4e9e6516736927fb0
Alex Yu <dtnvsy6@ucl.ac.uk>
2022-10-27 23:56:53 +0100

    alex_mechanism_workingbackup

---
+++ Assets/Scripts/mapGenerator/mapArray.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapArray : MonoBehaviour
+{
+    
+public GameObject [] mapList;
+
+}
---
+++ Assets/Scripts/mapGenerator/mapGenerator_back.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapGenerator_back : MonoBehaviour
+{
+    // GameObject outcomeMap;
+
+    // void pickRandomMap()
+    // {
+    //     index = Random.Range (0, mapList.Length)
+    //     outcomeMap = mapList[index];
+    // }
+
+    // void addMapp()
+    // {
+    //     transform thisLocation = transform.position;
+    // }
+
+    // void onCollisionEnger(Collision col)
+    // {  
+    //     if (col.gameObject.tag == "Player")
+    //     {
+    //         add map
+
+    //     }
+    // }
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
